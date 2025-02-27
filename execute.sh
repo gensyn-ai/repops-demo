acaf2b93b6ae9c4e50789af66864fee2fc156c1e
Christopher Nies <cniessigma@gmail.com>
2025-02-27 09:06:03 -0800

    Slightly less cryptic OOM error

--- execute.sh
+++ execute.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eou pipefail
+
 if [ -f /home/gensyn/.profile ]; then
    # shellcheck source=/dev/null
    source /home/gensyn/.profile
@@ -9,4 +11,4 @@ if [ ! -f ./Llama-3.2-1B-Instruct/model.safetensors ]; then
    ./download-llama.sh
 fi
 
-exec python repops-demo.py
+python repops-demo.py
